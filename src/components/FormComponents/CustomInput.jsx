6c8911b5aee8203dfc8f60fca25535378d61cdd6
emil backlund <emil_backi_@hotmail.com>
2023-05-07 00:22:57 +0200

    hmm making new branch if I need to go back

--- src/components/FormComponents/CustomInput.jsx
+++ src/components/FormComponents/CustomInput.jsx
@@ -10,6 +10,7 @@ const CustomInput = ({
   indent = 'indent-1.5',
   required = ' *',
   flex1,
+  position,
 }) => {
   return (
     <div className={`${flex1}`}>
@@ -27,7 +28,7 @@ const CustomInput = ({
           id={name}
           placeholder={placeholder}
           className={`
-          ${height} ${indent} ${shadow} 
+          ${height} ${indent} ${shadow} ${position}
           block w-full rounded-md border-0 py-1.5 text-gray-900 
           ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 
           focus:ring-inset focus:ring-secondaryOrange sm:text-sm sm:leading-6
--- src/pages/RegisterVenuePage/Stage3.jsx
+++ src/pages/RegisterVenuePage/Stage3.jsx
@@ -1,11 +1,61 @@
+import { useEffect, useRef } from 'react';
 import StageTemplate from './StageTemplate';
-import CustomInput from '../../components/FormComponents/CustomInput';
 
 const Stage3 = () => {
+  const mapRef = useRef(null);
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    let map;
+
+    async function initMap() {
+      const position = { lat: 60.128, lng: 18.643 };
+      const { Map, places } = await google.maps.importLibrary('maps', 'places');
+
+      map = new Map(mapRef.current, {
+        zoom: 3,
+        center: position,
+        mapId: 'DEMO_MAP_ID',
+        disableDefaultUI: true,
+      });
+
+      const autocomplete = new places.Autocomplete(inputRef.current);
+      autocomplete.bindTo('bounds', map);
+
+      autocomplete.addListener('place_changed', () => {
+        const place = autocomplete.getPlace();
+        if (!place.geometry) {
+          window.alert("No details available for input: '" + place.name + "'");
+          return;
+        }
+
+        if (place.geometry.viewport) {
+          map.fitBounds(place.geometry.viewport);
+        } else {
+          map.setCenter(place.geometry.location);
+          map.setZoom(17);
+        }
+      });
+    }
+    initMap();
+  }, []);
+
   return (
     <div>
       <StageTemplate stageNumber={3} stageTitle={'Location'} />
-      <div className="flex flex-col gap-4"></div>
+      <div className="w-full relative">
+        <div
+          ref={mapRef}
+          className="flex flex-col gap-4 w-full h-[400px] rounded-3xl "
+        ></div>
+        <input
+          ref={inputRef}
+          id="locationInput"
+          type="text"
+          placeholder="Enter a location"
+          className="w-11/12 p-4 absolute left-1/2 -translate-x-1/2 right-0 top-4 h-12 rounded-3xl"
+        />
+      </div>
     </div>
   );
 };
